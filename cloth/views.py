8fd77755122a89229d1e7e96df6303b57d802736
mego354 <143918350+mego354@users.noreply.github.com>
2024-12-30 00:17:26 +0200

    first commit

--- cloth/views.py
+++ cloth/views.py
@@ -4,23 +4,23 @@ from .models import Fabric,CutTransfer,ReturnTransfer,Statistics, Updates
 def fabric_view(request):
     return render(request, "cloth/balance.html", {
         'models': Fabric.objects.all().order_by('-date'),
-        'update': Updates.objects.first()
+        'update': Updates.objects.all().order_by('-date').first()
     })
 
 def cut_transfer_view(request):
     return render(request, "cloth/cut.html", {
         'models': CutTransfer.objects.all().order_by('-date'),
-        'update': Updates.objects.first()
+        'update': Updates.objects.all().order_by('-date').first()
     })
 
 def return_transfer_view(request):
     return render(request, "cloth/return.html", {
         'models': ReturnTransfer.objects.all().order_by('-date'),
-        'update': Updates.objects.first()
+        'update': Updates.objects.all().order_by('-date').first()
     })
 
 def statistics_view(request):
     return render(request, "cloth/statistics.html", {
         'models': Statistics.objects.all().order_by('-date'),
-        'update': Updates.objects.first()
+        'update': Updates.objects.all().order_by('-date').first()
     })
