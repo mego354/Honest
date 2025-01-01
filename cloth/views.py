3f5b3823449a0284d9e93dca517c0c0830ae5538
mego354 <143918350+mego354@users.noreply.github.com>
2025-01-01 21:14:00 +0200

    dynamic filter fields

--- cloth/views.py
+++ cloth/views.py
@@ -4,25 +4,23 @@ from .models import Fabric, CutTransfer, ReturnTransfer, Statistics, Updates
 
 class FilterableListView(ListView):
     template_name = None
-    model = None 
-    filter_field_1 = 'fabric_code'
-    filter_field_2 = 'model_number'
+    model = None
+    filter_fields = []  # To be defined in the child views
     context_object_name = 'models'
 
     def get_queryset(self):
         queryset = super().get_queryset()
         filters = Q()
-        ordering = self.request.GET.get('order', 'desc')  # Default to descending
+
+        # Get the ordering parameter from the GET request, default to descending
+        ordering = self.request.GET.get('order', 'desc')
         order_by_field = '-date' if ordering == 'desc' else 'date'
 
-        # Apply filters for the specified fields
-        filter_value_1 = self.request.GET.get(self.filter_field_1)
-        filter_value_2 = self.request.GET.get(self.filter_field_2)
-
-        if filter_value_1:
-            filters &= Q(**{f"{self.filter_field_1}__icontains": filter_value_1})
-        if filter_value_2:
-            filters &= Q(**{f"{self.filter_field_2}__icontains": filter_value_2})
+        # Loop through each filter field and apply it dynamically
+        for field in self.filter_fields:
+            filter_value = self.request.GET.get(field)
+            if filter_value:
+                filters &= Q(**{f"{field}__icontains": filter_value})
 
         return queryset.filter(filters).order_by(order_by_field)
 
@@ -33,33 +31,35 @@ class FilterableListView(ListView):
         update = Updates.objects.all().order_by('-date').first()
         context['update'] = update.date.strftime("%d/%m %I:%M %p") if update and update.date else None
         
-        # Retrieve verbose names for the filter fields
+        # Retrieve verbose names for the filter fields dynamically
         model_meta = self.model._meta
-        context['filter_field_1'] = {
-            'field_name': self.filter_field_1,
-            'verbose_name': model_meta.get_field(self.filter_field_1).verbose_name,
-            'value': self.request.GET.get(self.filter_field_1) or "",
-        }
-        context['filter_field_2'] = {
-            'field_name': self.filter_field_2,
-            'verbose_name': model_meta.get_field(self.filter_field_2).verbose_name,
-            'value': self.request.GET.get(self.filter_field_2) or "",
-        }
+        context['filter_fields'] = [
+            {
+                'field_name': field,
+                'verbose_name': model_meta.get_field(field).verbose_name,
+                'value': self.request.GET.get(field) or "",
+            }
+            for field in self.filter_fields
+        ]
+        
         return context
 
 class FabricView(FilterableListView):
     template_name = "cloth/balance.html"
     model = Fabric
-    filter_field_2 = 'fabric_name'
+    filter_fields = ['fabric_code', 'fabric_name', 'color']
 
 class CutTransferView(FilterableListView):
     template_name = "cloth/cut.html"
     model = CutTransfer
+    filter_fields = ['fabric_code', 'model_number']
 
 class ReturnTransferView(FilterableListView):
     template_name = "cloth/return.html"
     model = ReturnTransfer
+    filter_fields = ['fabric_code', 'fabric_name']
 
 class StatisticsView(FilterableListView):
     template_name = "cloth/statistics.html"
     model = Statistics
+    filter_fields = ['fabric_code', 'model_number']
