9a8686eddb6ad8ef0a0d8d82b2fa874800f25b00
mego354 <143918350+mego354@users.noreply.github.com>
2025-01-25 13:19:07 +0200

    size/amount edit done

--- cloth/urls.py
+++ cloth/urls.py
@@ -5,11 +5,12 @@ from .views import *
 from .api import PopulateModelsView
 
 urlpatterns = [
-    path('api/populate-models/', PopulateModelsView.as_view(), name='populate-models'),
-    path('generate_pdf/', generate_pdf, name='generate_pdf'),
     path('', FabricView.as_view(), name='fabric_view'),
     path('CutTransfer/', CutTransferView.as_view(), name='cut_transfer_view'),
     path('ReturnTransfer/', ReturnTransferView.as_view(), name='return_transfer_view'),
     path('Statistics/', StatisticsView.as_view(), name='statistics_view'),
+    
+    path('api/populate-models/', PopulateModelsView.as_view(), name='populate-models'),
+    path('generate_pdf/', generate_pdf, name='generate_pdf'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cloth/', include('cloth.urls')),
-    path('', include('production.urls')),
+    path('production/', include('production.urls')),
 ]
