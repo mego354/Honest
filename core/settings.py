25c50cd1cf50f175cda053986b1b7d1ac963495e
mego354 <143918350+mego354@users.noreply.github.com>
2025-02-06 11:52:04 +0200

    edit ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ Hosted = True
 # Hosted = False 
 if Hosted:
     DEBUG = False
-    ALLOWED_HOSTS = ['honestfabrics.pythonanywhere.com', 'honestfactory.top']
+    ALLOWED_HOSTS = ['honestfabrics.pythonanywhere.com', 'www.honestfactory.top']
     STATIC_URL = 'static/'
     STATIC_ROOT = '/home/honestfabrics/Honest/cloth/static'
 
