f815d7677fbfab95eecbfb8bcf17d1e8bf6ba484
mego354 <143918350+mego354@users.noreply.github.com>
2025-02-10 05:16:47 +0200

    edit percentage over y

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from cloth.views import FabricView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', FabricView.as_view(), name='fabric_view'),
+    # path('', FabricView.as_view(), name='fabric_view'),
     path('cloth/', include('cloth.urls')),
     path('production/', include('production.urls')),
 ]
