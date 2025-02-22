0b479924067c0c3e16d29e0206c29f5c28a04bab
mego354 <143918350+mego354@users.noreply.github.com>
2025-02-22 07:36:07 +0200

    update backup

--- cloth/serializers.py
+++ cloth/serializers.py
@@ -2,29 +2,74 @@ from rest_framework import serializers
 from .models import Fabric, CutTransfer, ReturnTransfer, Statistics
 
 class FabricSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
-
     class Meta:
         model = Fabric
         fields = '__all__'
 
-class CutTransferSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
+    def create(self, validated_data):
+        instance, created = Fabric.objects.get_or_create(
+            fabric_code=validated_data.get('fabric_code'),
+            defaults=validated_data  # Use provided data if creating a new entry
+        )
+        if not created:
+            # Update existing entry
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+        return instance
 
+class CutTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = CutTransfer
         fields = '__all__'
 
-class ReturnTransferSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
+    def create(self, validated_data):
+        # Ensure ID is not manually assigned
+        validated_data.pop("id", None)
 
+        instance, created = CutTransfer.objects.get_or_create(
+            fabric_code=validated_data.get('fabric_code'),
+            model_number=validated_data.get('model_number'),
+            defaults=validated_data
+        )
+        if not created:
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+        return instance
+
+class ReturnTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReturnTransfer
         fields = '__all__'
 
-class StatisticsSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
+    def create(self, validated_data):
+        validated_data.pop("id", None)  # Remove manually assigned ID
 
+        instance, created = ReturnTransfer.objects.get_or_create(
+            fabric_code=validated_data.get('fabric_code'),
+            model_number=validated_data.get('model_number'),
+            defaults=validated_data
+        )
+        if not created:
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+        return instance
+
+class StatisticsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Statistics
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data.pop("id", None)  # Remove manually assigned ID
+
+        instance, created = Statistics.objects.get_or_create(
+            defaults=validated_data
+        )
+        if not created:
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+        return instance
