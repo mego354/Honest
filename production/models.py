d6ed18d47515dd12b016cf4c33be6e022dd1c2d8
mego354 <143918350+mego354@users.noreply.github.com>
2025-02-23 10:11:20 +0200

    update production form

--- production/models.py
+++ production/models.py
@@ -57,6 +57,8 @@ class Model(models.Model):
         super().save(*args, **kwargs)
     
     def get_total_available_carton(self):
+        # if self.model_number == '36DG':
+        #     print(SizeAmount.objects.filter(model=self).values_list('amount', 'Packing_per_carton'))
         return int(SizeAmount.objects.filter(model=self).aggregate(
             total_cartons=Sum(Cast(F('amount'), FloatField()) / Cast(F('Packing_per_carton'), FloatField()))
         )['total_cartons'] or 0)
